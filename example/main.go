4932cf761345e76aa2f61b1107bcc5d7f7d6a151
aklin <490191132@qq.com>
2024-04-28 09:55:57 +0800

    修改main.go

--- example/main.go
+++ example/main.go
@@ -18,10 +18,10 @@ var (
 
 func main() {
 
-	flag.StringVar(&DstIP, "d", "111.111.111.111", "目标IP地址")                               // 有检查
-	flag.StringVar(&GW, "g", "192.168.0.1", "本地网关IP地址")                                  // 用户输错网关ip，用户承担风险
+	flag.StringVar(&DstIP, "d", "111.111.111.111", "目标IP地址")                         // 有检查
+	flag.StringVar(&GW, "g", "192.168.0.1", "本地网关IP地址")                              // 用户输错网关ip，用户承担风险
 	flag.StringVar(&SrcIP, "s", "192.168.0.2/24", "本机IP地址,需为CIDR表示法如192.168.0.2/24") // 有检查
-	flag.StringVar(&port, "p", "79-81", "目标端口范围")                                        //  有检查
+	flag.StringVar(&port, "p", "79-81", "目标端口范围")                                    //  有检查
 	flag.BoolVar(&help, "help", false, "Usage: show help message")
 	flag.Parse()
 
@@ -38,6 +38,8 @@ func main() {
 	}
 
 	for _, ip := range ipResult {
+		fmt.Println("--------------------------")
+		fmt.Printf("scanner ip:%v\n", ip)
 		err := ppp.SynScan(ip, GW, SrcIP, portResult)
 		if err != nil {
 			fmt.Println(err)
